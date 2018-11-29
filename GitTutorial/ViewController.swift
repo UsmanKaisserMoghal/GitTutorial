8e69470ec74662dc5f8a072b6960c487fb370740
Usman <usmank.moghal@gmail.com>
2018-11-29 10:07:22 +0500

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by iMAC2 on 29/11/2018.
+//  Copyright © 2018 FAST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
